981665d4cbeb986a0807a301a4ca223356562b38
aaljaseer <63748449+aaljaseer@users.noreply.github.com>
2020-04-15 22:47:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let id="";
+let bref="abdulelah and saif come way back since high school Graduated  2014/n abdulkareem joined college aboard.Howver he trasfered college credit and enrolld at YU ,Spring 2019./n thats where thamer and kareem met on accounting course and hit it off /n Thamer started abdulelah and saif come way back since high school Graduated  2014/n abdulkareem joined college aboard.Howver he trasfered college credit and enrolld at YU ,Spring 2019./n thats where thamer and kareem met on accounting course and hit it off /n Thamer started  2018 on Accounting Major . thamer have strong believe that most if not all HS senior dislike the area of accouning yayying eazy A's major .'";
+
+let studentAr= new Array[3];
+studentAr{
+    name;"",
+    id;"",
+    major;"",
+    name;"",
+    id;"",
+    major;"",
+    name;"",
+    id;"",
+    major;"",
+
+
+}
+
+{
+
+}
+
